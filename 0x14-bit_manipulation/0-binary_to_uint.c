f53283a9ad77e9d07ff39c1f8a11fdbf67ecfc5b
JACK OLANG <olangjack@yahoo.com>
2023-10-26 13:27:39 +0300

    0-binary_to_unit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,23 +1,11 @@
-#include "main.h"
+#include <unistd.h>
 
 /**
- * binary_to_uint - converts a binary number to unsigned int
- * @b: string containing the binary number
- * Return: the converted number
+ * _putchar - Write a character to stdout
+ * @c: The character to print
+ *
+ * Return: 1 on success, -1 on error (and errno is set)
  */
-unsigned int binary_to_uint(const char *b)
-{
-	int i;
-	unsigned int dec_val = 0;
-
-	if (!b)
-		return (0);
-
-	for (i = 0; b[i]; i++)
-	{
-		if (b[i] < '0' || b[i] > '1')
-			return (0);
-		dec_val = 2 * dec_val + (b[i] - '0');
-	}
-	return (dec_val);
+int _putchar(char c) {
+    return write(STDOUT_FILENO, &c, 1);
 }
