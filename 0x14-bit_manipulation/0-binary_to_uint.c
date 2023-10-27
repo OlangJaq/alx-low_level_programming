8a796af7d4dfdfb657628dbf8203253bf38d491a
JACK OLANG <olangjack@yahoo.com>
2023-10-27 07:41:45 +0300

    0-binary_to_unit.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,4 @@
-#include <stdiok.h>
+#include <stdio.h>
 #include <main.h>
 
 unsigned int binary_to_uint(const char *b)
@@ -22,16 +22,4 @@ unsigned int binary_to_uint(const char *b)
     }
 
     return result;
-}
-#include <stdio.h>
-#include <main.h>
-
-/**
- * _putchar - Write a character to stdout
- * @c: The character to print
- *
- * Return: 1 on success, -1 on error (and errno is set)
- */
-int _putchar(char c) {
-    return write(STDOUT_FILENO, &c, 1);
 }
